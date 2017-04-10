add615444abfaca750b27206075b5d4f53ab03ac
Blesa Patricia <blesapati@gmail.com>
2017-04-10 09:52:44 +0200

    feat(seconds) add a function, that make a new list with the proper elements, maybe need fix

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,10 @@
-def seconds(change_this):
-    pass
+def seconds(long_list):
+    short_list = []
+    for item in range(2, len(long_list), 2):
+        short_list.append(item)
+    return short_list
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
-print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+print(seconds([1, 2, 3, 4, 5]))
