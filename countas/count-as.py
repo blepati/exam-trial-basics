f47cba433a977a4323173484a245971816a55048
Blesa Patricia <blesapati@gmail.com>
2017-04-10 11:39:29 +0200

    feat(count-as) add function that count the a letters and have an error handling to FileNotFound

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,5 +1,18 @@
-def count_as(change_this):
-    pass
+def count_as(file_name):
+    try:
+        content_to_count = open(file_name)
+        letters_to_count = content_to_count.read()
+        num_of_as = 0
+        content_to_count.close()
+
+        for letter in letters_to_count.lower():
+            if letter == "a":
+                num_of_as += 1
+        return num_of_as
+
+    except FileNotFoundError:
+            return 0
+
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
